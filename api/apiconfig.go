2e1ce2a4233b2f59e2e4e7ad865a716c789fc29a
h4r5h1l <h4r5h1l@gmail.com>
2026-02-19 22:43:40 -0500

    architecture

---
+++ api/apiconfig.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+// ApiConfig is a struct that holds the hit count for the file server and provides methods to manage it
+type ApiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+// MiddlewareMetricInc is a HOF that increments the file server hit count and then calls the next handler in the chain
+func (cfg *ApiConfig) MiddlewareMetricInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// GetFileServerHits is an HTTP handler that responds with the current hit count for the file server
+func (cfg *ApiConfig) GetFileServerHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `<!doctype html>
+					<html>
+					<body>
+						<h1>Welcome, Chirpy Admin</h1>
+						<p>Chirpy has been visited %d times!</p>
+					</body>
+					</html>
+					`, cfg.fileserverHits.Load())
+}
+
+// ResetFileServerHits resets the hit count and returns an HTML page showing the new value.
+func (cfg *ApiConfig) ResetFileServerHits(w http.ResponseWriter, r *http.Request) {
+    cfg.fileserverHits.Store(0)
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    fmt.Fprintf(w, `<!doctype html>
+					<html>
+					<body>
+						<h1>Welcome, Chirpy Admin</h1>
+						<p>Chirpy has been visited %d times!</p>
+					</body>
+					</html>
+					`, cfg.fileserverHits.Load())
+}
--- server.go
+++ server.go
@@ -1,23 +1,38 @@
 package main
 
 import (
+	"github.com/h4r5h1l/Chirpy/api"
 	"net/http"
 )
 
 func main() {
+	// Create a new ServeMux to handle incoming HTTP requests
 	mux := http.NewServeMux()
+	// Create a new HTTP server with the specified address and handler
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	// Create a file server to serve files from the current directory, stripping the "/app" prefix from the URL path
 	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
-	mux.Handle("/app/", fileServer)
-	mux.HandleFunc("/healthz", readyCheckHandler)
+	// Create a new apiConfig instance to manage the file server hit metrics
+	apiCfg := &api.ApiConfig{}
+	// Register the file server handler with the middleware to increment the hit count
+	mux.Handle("/app/", apiCfg.MiddlewareMetricInc(fileServer))
+	// Register handlers for the metrics and reset endpoints
+	mux.HandleFunc("GET /admin/metrics", apiCfg.GetFileServerHits)
+	mux.HandleFunc("POST /admin/reset", apiCfg.ResetFileServerHits)
+	// Register a handler for the health check endpoint
+	mux.HandleFunc("GET /healthz", readyCheckHandler)
+	// Start the HTTP server and listen for incoming requests
 	server.ListenAndServe()
 }
 
+// readyCheckHandler is a simple HTTP handler that responds with "OK" to indicate that the server is ready to handle requests
 func readyCheckHandler(w http.ResponseWriter, r *http.Request) {
+	// Set the Content-Type header to indicate that the response is plain text
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	// Write the HTTP status code and response body to indicate that the server is healthy
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
