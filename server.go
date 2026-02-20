584240546f2bd45600205cd1d891ce09cc646271
h4r5h1l <h4r5h1l@gmail.com>
2026-02-19 20:16:34 -0500

    Custom handlers

--- server.go
+++ server.go
@@ -10,5 +10,14 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", fileServer)
+	mux.HandleFunc("/healthz", readyCheckHandler)
 	server.ListenAndServe()
+}
+
+func readyCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
